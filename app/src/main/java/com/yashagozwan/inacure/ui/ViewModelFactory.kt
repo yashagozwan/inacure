1e9c10dce79bfe5777d35717b564ee9d8c016dd1
yashagozwan <yashagozwan13@gmail.com>
2022-05-15 18:31:36 +0700

    add viewBinding setup

---
+++ app/src/main/java/com/yashagozwan/inacure/ui/ViewModelFactory.kt
@@ -0,0 +1,3 @@
+package com.yashagozwan.inacure.ui
+
+class ViewModelFactory {}
---
+++ app/src/main/java/com/yashagozwan/inacure/ui/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.yashagozwan.inacure.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yashagozwan.inacure.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
