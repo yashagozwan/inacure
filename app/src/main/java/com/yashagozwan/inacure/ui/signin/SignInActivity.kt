f08360f77bed0d3f85b284a9b41fb06631b15e7b
yashagozwan <yashagozwan13@gmail.com>
2022-05-27 23:53:56 +0700

    add validation sign in and sign up

--- app/src/main/java/com/yashagozwan/inacure/ui/signin/SignInActivity.kt
+++ app/src/main/java/com/yashagozwan/inacure/ui/signin/SignInActivity.kt
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.widget.doOnTextChanged
 import com.yashagozwan.inacure.R
 import com.yashagozwan.inacure.databinding.ActivitySignInBinding
 import com.yashagozwan.inacure.model.SignIn
@@ -19,6 +21,8 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var viewBinding: ActivitySignInBinding
     private val factory = ViewModelFactory.getInstance(this)
     private val viewModel: SignInViewModel by viewModels { factory }
+    private var isValidEmail = false
+    private var isValidPassword = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +31,7 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
         hideAppBar()
         customStatusBar()
         addButtonListener()
+        handleErrorField()
     }
 
     private fun hideAppBar() {
@@ -52,41 +57,75 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private fun handleErrorField() {
+        viewBinding.editEmail.doOnTextChanged { text, _, _, _ ->
+            if (text!!.isEmpty()) {
+                viewBinding.tilEmail.isErrorEnabled = true
+                viewBinding.tilEmail.error = "Email not allowed empty"
+                isValidEmail = false
+            } else if (!text.contains("@") || !text.contains(".")) {
+                viewBinding.tilEmail.isErrorEnabled = true
+                viewBinding.tilEmail.error = "Please enter valid email"
+                isValidEmail = false
+            } else {
+                viewBinding.tilEmail.isErrorEnabled = false
+                viewBinding.tilEmail.error = null
+                isValidEmail = true
+            }
+        }
+
+        viewBinding.editPassword.doOnTextChanged { text, _, _, _ ->
+            if (text!!.isEmpty()) {
+                viewBinding.tilPassword.isErrorEnabled = true
+                viewBinding.tilPassword.error = "Password not allowed empty"
+                isValidPassword = false
+            } else {
+                viewBinding.tilPassword.isErrorEnabled = false
+                viewBinding.tilPassword.error = null
+                isValidPassword = true
+            }
+        }
+    }
+
     private fun signIn() {
         val email = viewBinding.editEmail.text.toString().trim()
         val password = viewBinding.editPassword.text.toString().trim()
 
-        if (email.isEmpty()) {
-            viewBinding.editEmail.error = "Field not allowed empty"
-            return
-        }
-
-        if (password.isEmpty()) {
-            viewBinding.editPassword.error = "Field not allowed ampty"
-            return
-        }
-
-        val signIn = SignIn(email, password)
-        viewModel.signIn(signIn).observe(this) {
-            when (it) {
-                is Result.Loading -> {
-                    viewBinding.cvLoading.visibility = View.VISIBLE
-                }
-                is Result.Success -> {
-                    viewBinding.cvLoading.visibility = View.GONE
-                    val response = it.data
-                    val token = response.data.token
-                    viewModel.saveToken(token)
-                    Intent(this@SignInActivity, MainActivity::class.java).also { startActivity(it) }
-                    finish()
-                }
-                is Result.Error -> {
-                    viewBinding.cvLoading.visibility = View.GONE
-                    val error = it.error
-                    Log.d(TAG, error)
+        if (isValidEmail && isValidPassword) {
+            val signIn = SignIn(email, password)
+            viewModel.signIn(signIn).observe(this) { result ->
+                when (result) {
+                    is Result.Loading -> {
+                        viewBinding.cvLoading.visibility = View.VISIBLE
+                        viewBinding.tvInvalidAccount.visibility = View.GONE
+                    }
+                    is Result.Success -> {
+                        viewBinding.cvLoading.visibility = View.GONE
+                        viewBinding.tvInvalidAccount.visibility = View.GONE
+                        val response = result.data
+                        val token = response.data.token
+                        viewModel.saveToken(token)
+                        Intent(this@SignInActivity, MainActivity::class.java).also { intent ->
+                            startActivity(intent)
+                        }
+                        finish()
+                        showToast("Sign In ...")
+                    }
+                    is Result.Error -> {
+                        viewBinding.cvLoading.visibility = View.GONE
+                        viewBinding.tvInvalidAccount.visibility = View.VISIBLE
+                        val error = result.error
+                        Log.d(TAG, error)
+                    }
                 }
             }
+        } else {
+            showToast("Please fill in all the fields")
         }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this@SignInActivity, message, Toast.LENGTH_SHORT).show()
     }
 
     companion object {
--- app/src/main/java/com/yashagozwan/inacure/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/yashagozwan/inacure/ui/signup/SignUpActivity.kt
@@ -4,19 +4,25 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import com.yashagozwan.inacure.R
 import com.yashagozwan.inacure.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var viewBinding: ActivitySignUpBinding
+    private var isValidName: Boolean = false
+    private var isValidEmail: Boolean = false
+    private var isValidPassword: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        viewBinding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
         hideAppBar()
         customStatusBar()
         addButtonListener()
+        handleErrorField()
     }
 
     private fun hideAppBar() {
@@ -31,12 +37,77 @@ class SignUpActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun addButtonListener() {
-        binding.tvSignIn.setOnClickListener(this)
+        viewBinding.tvSignIn.setOnClickListener(this)
+        viewBinding.btnSignUp.setOnClickListener(this)
+    }
+
+    private fun handleErrorField() {
+        viewBinding.editName.doOnTextChanged { text, _, _, _ ->
+            if (text!!.isEmpty()) {
+                viewBinding.tilName.isErrorEnabled = true
+                viewBinding.tilName.error = "Name not allowed empty"
+                isValidName = false
+            } else if (text.length < 3) {
+                viewBinding.tilName.isErrorEnabled = true
+                viewBinding.tilName.error = "Name min length 3 character"
+                isValidName = false
+            } else {
+                viewBinding.tilName.isErrorEnabled = false
+                viewBinding.tilName.error = null
+                isValidName = true
+            }
+        }
+
+        viewBinding.editEmail.doOnTextChanged { text, _, _, _ ->
+            if (text!!.isEmpty()) {
+                viewBinding.tilEmail.isErrorEnabled = true
+                viewBinding.tilEmail.error = "Email not allowed empty"
+                isValidEmail = false
+            } else if (!text.contains("@") || !text.contains(".")) {
+                viewBinding.tilEmail.isErrorEnabled = true
+                viewBinding.tilEmail.error = "Please enter valid email"
+                isValidEmail = false
+            } else {
+                viewBinding.tilEmail.isErrorEnabled = false
+                viewBinding.tilEmail.error = null
+                isValidEmail = true
+            }
+        }
+
+        viewBinding.editPassword.doOnTextChanged { text, _, _, _ ->
+            if (text!!.isEmpty()) {
+                viewBinding.tilPassword.isErrorEnabled = true
+                viewBinding.tilPassword.error = "Password not allowed empty"
+                isValidPassword = false
+            } else if (text.length < 6) {
+                viewBinding.tilPassword.isErrorEnabled = true
+                viewBinding.tilPassword.error = "Password min length 6 character"
+                isValidPassword = false
+            } else {
+                viewBinding.tilPassword.isErrorEnabled = false
+                viewBinding.tilPassword.error = null
+                isValidPassword = true
+            }
+        }
+    }
+
+
+    private fun signUp() {
+        val name = viewBinding.editName.text.toString().trim()
+        val email = viewBinding.editEmail.text.toString().trim()
+        val password = viewBinding.editPassword.text.toString().trim()
+
+        if (isValidName && isValidEmail && isValidPassword) {
+            Toast.makeText(this, "$name, $email, $password", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onClick(view: View) {
         when (view.id) {
             R.id.tv_sign_in -> finish()
+            R.id.btn_sign_up -> signUp()
         }
     }
 }
